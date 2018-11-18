a9c2c54802850b7320ed3911166a22414279bb32
Claudio Matsuoka <cmatsuoka@gmail.com>
2018-11-18 10:58:34 -0200

    Change simple example to write to a wav file
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- examples/simple/main.cpp
+++ examples/simple/main.cpp
@@ -5,11 +5,23 @@
 #include <sys/stat.h>
 #include <cstdint>
 #include <cstring>
+#include <cstdint>
 #include <iostream>
 #include <fstream>
 #include <haze.h>
 
 #define BUFFER_SIZE 4096
+
+// Example: play module and store a few buffers of data into a wav file.
+// The output file will be called out.wav.
+
+
+void write_le(std::ofstream& out, uint32_t v, int size)
+{
+    for (int i = 0; i < size; i++, v >>= 8) {
+        out.put(v & 0xff);
+    }
+}
 
 
 int main(int argc, char** argv)
@@ -41,7 +53,6 @@ int main(int argc, char** argv)
         exit(EXIT_FAILURE);
     }
 
-
     // probe module format
     haze::ModuleInfo mi;
     if (!haze::probe(data, size, mi)) {
@@ -60,15 +71,34 @@ int main(int argc, char** argv)
     hz.player_info(pi);
     std::cout << "Player  : " << pi.name << std::endl;
 
+    // write wav file
+    std::ofstream outfile("out.wav", std::ios::out | std::ios::binary);
+    outfile << "RIFF....WAVEfmt ";
+    write_le(outfile, 16, 4);         // fmt chunk size
+    write_le(outfile, 1, 2);          // data format
+    write_le(outfile, 2, 2);          // channels
+    write_le(outfile, 44100, 4);      // sampling rate
+    write_le(outfile, 44100 * 4, 4);  // byte rate
+    write_le(outfile, 4, 2);          // frame size
+    write_le(outfile, 16, 2);         // sample size
+    outfile << "data....";
 
     // play module
+    std::cout << "Render buffer " << std::flush;
     int16_t *buffer = new int16_t[BUFFER_SIZE];
-
-    std::ofstream outfile("out.raw", std::ios::out | std::ios::binary);
     for (int i = 0; i < 400; i++) {
         hz.fill(buffer, BUFFER_SIZE);
         outfile.write(reinterpret_cast<char *>(buffer), BUFFER_SIZE);
+        std::cout << "." << std::flush;
     }
+    std::cout << std::endl;
+
+    uint32_t len = outfile.tellp();
+    outfile.seekp(4);
+    write_le(outfile, len - 8, 4);
+    outfile.seekp(40);
+    write_le(outfile, len - 32, 4);
+
     outfile.close();
 
     delete [] buffer;
